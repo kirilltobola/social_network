aca4f7f7ed8b5eca71e63913865b31ca207a65dd
Kirill Tobola <kirilltobola@gmail.com>
2022-08-21 22:16:15 +0800

    extend project's urls

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,21 +1,30 @@
-"""social_network URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.flatpages import views
+from django.urls import include, path
+
+handler404 = "posts.views.page_not_found" # noqa
+handler500 = "posts.views.server_error" # noqa
 
 urlpatterns = [
+    path('404/', include('posts.urls')),
+    path('500/', include('posts.urls')),
     path('admin/', admin.site.urls),
+    path('about/', include('django.contrib.flatpages.urls')),
+    path('about-author/', views.flatpage, {'url': '/about-author/'}, name='author'),
+    path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls')),
 ]
+
+urlpatterns += [
+    path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+    path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)), )
